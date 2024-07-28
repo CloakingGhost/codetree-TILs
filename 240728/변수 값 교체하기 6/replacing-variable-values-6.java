3433d3ec7717dc83005622a3c8e87b2530f76734
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2024-07-28 12:32:31 +0900

    Title: 변수 값 교체하기 6, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240728/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        swap(2,5);
+
+    }
+    private static int[] swap(int a, int b){
+        int tmp = a;
+        a= b;
+        b= tmp;
+        System.out.printf("%d\n%d",a,b);
+    }
+}
