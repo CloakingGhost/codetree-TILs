c606472d55206e55b5be031e1fa44a2883108729
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2024-07-28 12:07:49 +0900

    Title: 세 정수형 변수 선언, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240728/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        byte a = 7;
+        byte b = 23;
+        byte c = 30;
+
+        System.out.printf("%d + %d = %d", a, b, c);
+
+    }
+}
