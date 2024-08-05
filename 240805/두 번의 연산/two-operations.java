8d3406a90c7311242b90652f26d0ed2101ac0393
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2024-08-05 16:11:44 +0900

    Title: 두 번의 연산, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240805/두 번의 연산/two-operations.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = new java.util.Scanner(System.in).nextInt();
+        if(a % 2 == 1){
+            a += 3;
+            if(a % 3 == 0){
+                a /= 3;
+            }
+        }
+        System.out.println(a);
+    }
+}
