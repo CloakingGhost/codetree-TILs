631958d24cd19090ba3ed5fd18790defa531a502
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2024-08-05 16:13:37 +0900

    Title: 두 번의 연산, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240805/두 번의 연산/two-operations.java
+++ 240805/두 번의 연산/two-operations.java
@@ -3,10 +3,10 @@ public class Main {
         int a = new java.util.Scanner(System.in).nextInt();
         if(a % 2 == 1){
             a += 3;
+        }
             if(a % 3 == 0){
                 a /= 3;
             }
-        }
         System.out.println(a);
     }
 }
