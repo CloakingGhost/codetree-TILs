830697d077019b7c604eb3070769c73a98c315a6
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2024-08-05 16:06:37 +0900

    Title: 살 수 있는 물건, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240805/살 수 있는 물건/things-able-to-buy.java
+++ 240805/살 수 있는 물건/things-able-to-buy.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         int n = new java.util.Scanner(System.in).nextInt();
-        System.out.printf("%s", n < 1000 ? "no" : n < 3000 : "mask" : "book");
+        System.out.printf("%s", n < 1000 ? "no" : n < 3000 ? "mask" : "book");
     }
 }
