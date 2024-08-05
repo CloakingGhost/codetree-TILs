4e3c5509e32d4b6e26ab0ca9a512d2711b6ae623
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2024-08-05 15:53:18 +0900

    Title: 삼항연산자 2, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240805/삼항연산자 2/ternary-operator-2.java
+++ 240805/삼항연산자 2/ternary-operator-2.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("%s",new java.util.Scanner(System.in).nextInt() == 1 ? "1" : "t");
+        System.out.printf("%s",new java.util.Scanner(System.in).nextInt() == 1 ? "t" : "f");
     }
 }
