f4863802978b500d76ad2a82502da8d500bb7b8d
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2024-08-05 17:02:45 +0900

    Title: 별 n개 출력하기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240805/별 n개 출력하기/print-n-stars.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = new java.util.Scanner(System.in).nextInt();
+
+        int i = 0;
+        while(i < 5){
+            System.out.println("*");
+            i++;
+        }
+    }
+}
