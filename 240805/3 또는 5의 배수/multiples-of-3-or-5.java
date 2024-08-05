e7285e98c0b6f86737109bc98c1ce1127910ad98
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2024-08-05 16:27:12 +0900

    Title: 3 또는 5의 배수, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240805/3 또는 5의 배수/multiples-of-3-or-5.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = new java.util.Scanner(System.in).nextInt();
+        if(a % 3 == 0) System.out.println("YES");
+        else  System.out.println("NO");
+        if(a % 5 == 0) System.out.println("YES");
+        else  System.out.println("NO");
+    }
+}
